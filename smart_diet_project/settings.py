2c232c193d083f3f5ea1996e47a7dee73991a87c
oladipupo samuel <samueltoluwani169@gmail.com>
2026-07-24 18:48:20 +0100

    Configure app for Render deployment

--- smart_diet_project/settings.py
+++ smart_diet_project/settings.py
@@ -1,3 +1,5 @@
+import os
+import dj_database_url
 """
 Django settings for smart_diet_project project.
 
@@ -20,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g&i*h3fa!$7pa8s754=!21q3@g8$#+i-11a1th_95p90u21c*$"
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-g&i*h3fa!$7pa8s754=!21q3@g8$#+i-11a1th_95p90u21c*$')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -42,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware", 
     "django.middleware.csrf.CsrfViewMiddleware",
