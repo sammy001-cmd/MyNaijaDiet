bfb562808a70300ed49042bbf13ef8d018ab7a5c
oladipupo samuel <samueltoluwani169@gmail.com>
2026-07-24 19:22:51 +0100

    Set STATIC_ROOT for collectstatic and optimize requirements

--- smart_diet_project/settings.py
+++ smart_diet_project/settings.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+from pathlib import Path
 """
 Django settings for smart_diet_project project.
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
