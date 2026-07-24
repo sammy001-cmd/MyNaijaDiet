b846ad5af0f92f521e85510ea1cb93db50c4fe23
oladipupo samuel <samueltoluwani169@gmail.com>
2026-07-24 20:01:50 +0100

    Automate database seeding and superuser creation

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# 1. Load the meals dataset into the live database
+python manage.py load_meals
+
+# 2. Create the superuser automatically (the '|| true' part prevents the build from crashing on future deploys if the user already exists)
+python manage.py createsuperuser --noinput --username $sam0001 --email $samueltoluwani169@gmail.com|| true
