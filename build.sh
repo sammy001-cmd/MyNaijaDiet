4e39d492fb31f01641d8370438ec30b5c46c3678
oladipupo samuel <samueltoluwani169@gmail.com>
2026-07-24 20:18:54 +0100

    Update superuser creation in build script to use hardcoded credentials for testing

--- build.sh
+++ build.sh
@@ -10,20 +10,17 @@ python manage.py migrate
 python manage.py load_meals
 
 # 2. Create the superuser directly via the Django ORM
+
 python manage.py shell <<EOF
-import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-email = os.environ.get('ADMIN_EMAIL')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+email = "samueltoluwani169@gmail.com"
+password = "samnaija001"
 
-if email and password:
-    if not User.objects.filter(email=email).exists():
-        User.objects.create_superuser(email=samueltoluwani169@gmail.com', password=samnaija001)
-        print("✅ Superuser created successfully!")
-    else:
-        print("✅ Superuser already exists.")
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
+    print("✅ Superuser created successfully!")
 else:
-    print("⚠️ Missing environment variables. Superuser not created.")
+    print("✅ Superuser already exists.")
 EOF
